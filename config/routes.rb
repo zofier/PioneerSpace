d6881234dafecb54e56d33eb8218542bd8fff5b0
zofier <zof11@excite.co.jp>
2016-02-11 17:29:54 +0900

    指摘された点をすべて修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :proto, only: [:index, :new]
   resources :users, only: [:edit, :update, :show]#mypage
-  resources :top, only: :index
   root 'top#index'
 end
