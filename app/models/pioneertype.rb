7bc64eec33108d82e68dbca4584e8b582a0c7071
zofier <zof11@excite.co.jp>
2016-02-16 22:55:56 +0900

    改行をした。cssを直した。

--- app/models/pioneertype.rb
+++ app/models/pioneertype.rb
@@ -2,6 +2,6 @@ class Pioneertype < ActiveRecord::Base
 
   has_many :pioneertype_images
   belongs_to :user
-  accepts_nested_attributes_for :pioneertype_images
+  accepts_nested_attributes_for :pioneertype_images, reject_if: proc { |attributes| attributes['image'].blank? }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :avatar, :group, :profile, :work
 
   has_many :pioneertypes
-  belongs_to :pioneertype_image
 
   mount_uploader :avatar, AvatarUploader
 end
