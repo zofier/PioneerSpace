92188409bb5be1d94d14dfa580440a23b1ca9fa3
zofier <zof11@excite.co.jp>
2016-02-17 21:29:51 +0900

    pioneertype.rb: true or falseを表すメソッドには?を追加しなければならないため、追記

--- app/models/pioneertype.rb
+++ app/models/pioneertype.rb
@@ -2,9 +2,9 @@ class Pioneertype < ActiveRecord::Base
 
   has_many :pioneertype_images
   belongs_to :user
-  accepts_nested_attributes_for :pioneertype_images, reject_if: :image_blank
+  accepts_nested_attributes_for :pioneertype_images, reject_if: :image_blank?
 
-    def image_blank(attributed)
+    def image_blank?(attributed)
       attributed['image'].blank?
     end
 
