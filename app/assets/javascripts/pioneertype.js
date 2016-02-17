ec87859cbbc51886b93dbbb1d4c4a7f983b7240c
zofier <zof11@excite.co.jp>
2016-02-17 19:40:49 +0900

    pioneer type.js: turbolinksがある状態でリロードせずとも画像を選択したら即時表示。

--- app/assets/javascripts/pioneertype.js
+++ app/assets/javascripts/pioneertype.js
@@ -1,6 +1,6 @@
 $(function() {
   // アップロードするファイルを選択
-  $('input[type=file]').change(function() {
+  $('input[type=file]').on("change", function() {
     var file = $(this).prop('files')[0];
 
     // 画像以外は処理を停止
@@ -12,20 +12,20 @@ $(function() {
     }
 
     // 画像表示
-    var $previewImageTag = $(this).parent()
+    var previewImageTag = $(this).parent()
 
     var reader = new FileReader();
     reader.onload = function() {
-      $previewImageTag.css("background-image" , "url(" + reader.result + ")");
-      $previewImageTag.css("background-size" , "contain");
+      previewImageTag.css("background-image" , "url(" + reader.result + ")");
+      previewImageTag.css("background-size" , "contain");
     }
 
     reader.readAsDataURL(file);
   });
 
   $(function(){
-    $(".image-upload-plus").click(function(){
-          $('.image-upload-plus').hide('span').removeClass('image-upload-plus');
+    $(".image-upload-plus").on("click", function(){
+          $('.image-upload-plus').fadeOut('image-upload-plus');
         });
   });
 });
