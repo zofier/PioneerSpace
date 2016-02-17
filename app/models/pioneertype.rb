51cdd08c2f06b71f8f5e6e61431e1029099e8e32
zofier <zof11@excite.co.jp>
2016-02-17 17:31:00 +0900

    pioneertype.rb: reject_if文を修正。_index.html.hamlの名前を_pioneertype.html.hamlに変更。それに伴い、index.html.hamlの記述を変更。show.html.hamlにて不要な式展開を短縮。

--- app/models/pioneertype.rb
+++ app/models/pioneertype.rb
@@ -2,6 +2,10 @@ class Pioneertype < ActiveRecord::Base
 
   has_many :pioneertype_images
   belongs_to :user
-  accepts_nested_attributes_for :pioneertype_images, reject_if: proc { |attributes| attributes['image'].blank? }
+  accepts_nested_attributes_for :pioneertype_images, reject_if: :reject_pioneertype_images
+
+    def reject_pioneertype_images(attributed)
+      attributed['image'].blank?
+    end
 
 end
