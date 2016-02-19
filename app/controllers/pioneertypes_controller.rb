704dd7d181d582ca339c8b32df1e9a2283ddfabc
hideya <zof11@excite.co.jp>
2016-02-19 12:34:08 +0900

    edit, update, destroyメソッド追加。

--- app/controllers/pioneertypes_controller.rb
+++ app/controllers/pioneertypes_controller.rb
@@ -18,9 +18,27 @@ class PioneertypesController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @pioneertype = Pioneertype.find(params[:id])
+  end
+
+  def update
+    current_user.pioneertype.update(update_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    pioneertype = Pioneertype.find(params[:id])
+    pioneertype.destroy
+    redirect_to root_path
+  end
+
   private
   def create_params
     params.require(:pioneertype).permit(:title, :concept, :catchcopy, pioneertype_images_attributes: [:id, :image, :status, :pioneertype_id]).merge(user_id: current_user.id)
   end
 
+  def update_params
+    params.require(:pioneertype).permit(:title, :concept, :catchcopy, pioneertype_images_attributes: [:id, :image, :status, :pioneertype_id]).merge(user_id: current_user.id)
+  end
 end
