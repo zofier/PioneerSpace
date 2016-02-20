1e14a0994fb6596fb40806ce07945f98dc7064e7
hideya <zof11@excite.co.jp>
2016-02-20 16:07:12 +0900

    綺麗なコードにするためにfind paramsを一緒くたに修正。

--- app/controllers/pioneertypes_controller.rb
+++ app/controllers/pioneertypes_controller.rb
@@ -1,4 +1,5 @@
 class PioneertypesController < ApplicationController
+before_action :set_pioneertype, only: [:destroy, :edit, :show]
 
   def index
     @pioneertypes = Pioneertype.page(params[:page]).per(8)
@@ -10,35 +11,32 @@ class PioneertypesController < ApplicationController
   end
 
   def show
-    @pioneertype = Pioneertype.find(params[:id])
   end
 
   def create
-    Pioneertype.create(create_params)
+    Pioneertype.create(pioneertype_params)
     redirect_to root_path
   end
 
   def edit
-    @pioneertype = Pioneertype.find(params[:id])
   end
 
   def update
-    current_user.pioneertype.update(update_params)
+    current_user.pioneertype.update(pioneertype_params)
     redirect_to root_path
   end
 
   def destroy
-    pioneertype = Pioneertype.find(params[:id])
-    pioneertype.destroy
+    @pioneertype.destroy
     redirect_to root_path
   end
 
   private
-  def create_params
+  def pioneertype_params
     params.require(:pioneertype).permit(:title, :concept, :catchcopy, pioneertype_images_attributes: [:id, :image, :status, :pioneertype_id]).merge(user_id: current_user.id)
   end
 
-  def update_params
-    params.require(:pioneertype).permit(:title, :concept, :catchcopy, pioneertype_images_attributes: [:id, :image, :status, :pioneertype_id]).merge(user_id: current_user.id)
+  def set_pioneertype
+    @pioneertype = Pioneertype.find(params[:id])
   end
 end
