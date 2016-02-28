53aef5d69a240180b5fb70bf56298cab1acdc7b8
hideya <zof11@excite.co.jp>
2016-02-28 13:58:23 +0900

    Kicked in comment associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :avatar, :group, :profile, :work
 
   has_many :pioneertypes
+  has_many :comments
 
   mount_uploader :avatar, AvatarUploader
 end
