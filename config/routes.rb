fc88efc86f4d3a51ddb0639f10785e52f38ba89f
hideya <zof11@excite.co.jp>
2016-02-28 14:00:39 +0900

    Defeated comment system.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :pioneertypes
+  resources :pioneertypes do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
   root 'pioneertypes#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212132451) do
+ActiveRecord::Schema.define(version: 20160227060846) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text",           limit: 65535
+    t.integer  "user_id",        limit: 4
+    t.integer  "pioneertype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pioneertype_images", force: :cascade do |t|
     t.string   "image",          limit: 255
