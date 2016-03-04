996e729c93ba258b6565613d8cb571f20f1cbabb
hideya <zof11@excite.co.jp>
2016-03-04 19:58:48 +0900

    Enabled to work newest/popular's sort buttons.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :pioneertypes do
     resources :comments, only: :create
+    collection do
+      get 'popular'
+      get 'newest'
+    end
   end
   resources :users, only: :show
   root 'pioneertypes#index'
