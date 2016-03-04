b21ddd20f13f3ec62b083560b70dc64640ad9a21
hideya <zof11@excite.co.jp>
2016-03-04 14:26:21 +0900

    Fixed undesirable find_by.

--- app/controllers/pioneertypes_controller.rb
+++ app/controllers/pioneertypes_controller.rb
@@ -13,7 +13,7 @@ before_action :set_pioneertype, only: [:destroy, :edit, :show, :update]
   def show
     @comment = Comment.new
     @likes = Like.where(pioneertype_id: params[:id])
-    @like = @pioneertype.likes.find_by(current_user.try(:id))
+    @like = @pioneertype.likes.find_by(user_id: current_user.try(:id))
   end
 
   def create
