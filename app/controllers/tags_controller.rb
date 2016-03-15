524c27fbb5392089d50d413cc027d6ca87b86974
wakuhide <zof11@excite.co.jp>
2016-03-15 12:06:42 +0900

    :golf:

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,8 +5,7 @@ class TagsController < ApplicationController
   end
 
   def show
-    @tag = params[:tag_name]
-    @tags = ActsAsTaggableOn::Tag.find_by(name: params[:tag_name])
+    @tag = ActsAsTaggableOn::Tag.find_by(name: params[:tag_name])
     @pioneertypes = Pioneertype.tagged_with(@tag).page(params[:page])
   end
 end
