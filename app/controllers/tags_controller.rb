e38d3b042a6372234c7327cebf30a0c14d816e65
wakuhide <zof11@excite.co.jp>
2016-03-15 17:10:13 +0900

    Kicked in per.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,6 +6,6 @@ class TagsController < ApplicationController
 
   def show
     @tag = ActsAsTaggableOn::Tag.find_by(name: params[:tag_name])
-    @pioneertypes = Pioneertype.tagged_with(@tag).page(params[:page])
+    @pioneertypes = Pioneertype.tagged_with(@tag).page(params[:page]).per(8)
   end
 end
